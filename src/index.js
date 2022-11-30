71f4eab6655dcf9c049862b6d5dfae9a272e40ce
Eyasu Teshome <joshtesh91@mail.com>
2022-12-01 02:03:15 +0800

    add fonteawesome to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
