35c11ad3be146343d42031a8c4606e965870594b
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-01 11:06:08 +0100

    Create bg

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -6,24 +6,32 @@ export default function SignUpPage() {
   const [password, setPassword] = useState("");
 
   return (
-    <div className="auth-page h-100 d-flex align-items-center justify-content-center">
-      <form>
-        <input
-          type="text"
-          value={name}
-          onClick={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={email}
-          onClick={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          value={password}
-          onClick={(e) => setPassword(e.target.value)}
-        />
-      </form>
+    <div className="background">
+      <div className="layer">
+        <div className="auth-page">
+          <h1>SIGN UP</h1>
+          <form>
+            <input
+              type="text"
+              value={name}
+              onClick={(e) => setName(e.target.value)}
+              placeholder="name"
+            />
+            <input
+              type="text"
+              value={email}
+              onClick={(e) => setEmail(e.target.value)}
+              placeholder="email"
+            />
+            <input
+              type="text"
+              value={password}
+              onClick={(e) => setPassword(e.target.value)}
+              placeholder="password"
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
