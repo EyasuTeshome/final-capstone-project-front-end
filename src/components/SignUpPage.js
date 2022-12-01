ec13a5b69b17bc57169b314d1ed783d6fe6f67a3
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-01 12:17:09 +0100

    Create handleSubmit

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,34 +1,68 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUpPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            name,
+            email,
+            password,
+          },
+        }),
+      });
+
+      // const resJson = await res.json();
+      if (res.status === 200) {
+        navigate("/");
+      } else {
+        alert("some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="background">
       <div className="layer">
         <div className="auth-page">
           <h1>SIGN UP</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               value={name}
-              onClick={(e) => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               placeholder="name"
             />
             <input
               type="text"
               value={email}
-              onClick={(e) => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="email"
             />
             <input
               type="text"
               value={password}
-              onClick={(e) => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="password"
             />
+
+            <button type="submit" className="form-btn">
+              NEXT
+            </button>
           </form>
         </div>
       </div>
