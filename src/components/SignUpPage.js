dbe5351033d78cb1061346bb63d88b1d6fd5a212
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-01 13:00:52 +0100

    Add form validation

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(true);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!name.trim() || password.length < 6) {
+      setIsValid(false);
+      return;
+    }
+
     try {
       const res = await fetch("http://localhost:3000/users", {
         method: "POST",
@@ -24,7 +31,6 @@ export default function SignUpPage() {
         }),
       });
 
-      // const resJson = await res.json();
       if (res.status === 200) {
         navigate("/");
       } else {
@@ -41,24 +47,36 @@ export default function SignUpPage() {
         <div className="auth-page">
           <h1>SIGN UP</h1>
           <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="name"
+                className={!isValid && !name.trim() ? "red-border" : ""}
+              />
+              {!isValid && !name.trim() && (
+                <span className="invalid-input">Can&apos;t be empty</span>
+              )}
+            </div>
             <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="name"
-            />
-            <input
-              type="text"
+              type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="email"
             />
-            <input
-              type="text"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="password"
-            />
+            <div className="input-container">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="password"
+                className={!isValid && password.length < 6 ? "red-border" : ""}
+              />
+              {!isValid && password.length < 6 && (
+                <span className="invalid-input">Must be over 6 charachters long</span>
+              )}
+            </div>
 
             <button type="submit" className="form-btn">
               NEXT
