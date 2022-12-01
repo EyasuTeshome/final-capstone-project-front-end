f81ec01d2318e8c16c48bec122c3f7b80ac63348
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-01 13:06:33 +0100

    Create route for navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Navbar from "./components/Navbar";
 import SignUpPage from "./components/SignUpPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-
       <Router>
         <Routes>
           <Route path="/sign_up" element={<SignUpPage />} />
-          <Route path="/sign_in" />
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<div>Home Page</div>} />
+          </Route>
         </Routes>
       </Router>
     </div>
