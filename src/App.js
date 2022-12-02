9f7506cdaefc6b001928cfed0c72f138d5f4c7e7
Eric <tuyishimireericc@gmail.com>
2022-12-02 07:02:14 +0200

    Added main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Slider from './components/Main/main';
+import Latest from './components/Main/latest';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="main-container">
+        <Slider />
+        <Latest />
+      </div>
     </div>
   );
 }
