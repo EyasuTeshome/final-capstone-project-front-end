126feb2e0336c36d901f3fc9374266485dd91e72
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-02 10:14:05 +0000

    Clean up code
    Create a container to hold the data

--- src/App.js
+++ src/App.js
@@ -5,32 +5,26 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-import Slider from './components/Main/main';
-import Latest from './components/Main/latest';
 import './App.css';
 
 import SignUpPage from './components/SignUpPage';
+import HomePage from './components/HomePage';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   const user = useSelector((state) => state.user);
 
   return (
     <div className="App">
-      <Navbar />
-      <div className="main-container">
-        <Slider />
-        <Latest />
-      </div>
       <Router>
         <Routes>
           <Route path="/sign_up" element={<SignUpPage />} />
           {/* If the user isnt logged it, redirect all urls to sign up page */}
           {user.data ? (
-            <Route path="/" element={<Navbar />}>
-              <Route index element={<div>Home Page</div>} />
-            </Route>
+            <>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/details" element={<DetailsPage />} />
+            </>
           ) : (
             <Route path="*" element={<Navigate to="/sign_up" />} />
           )}
