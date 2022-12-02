3f77d6e34b94383ddff042fe794fb9942e6abc73
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-02 09:51:10 +0000

    Setup container pages

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './Container';
+
+const HomePage = () => {
+  return (
+    <Container>
+      
+      </Container>
+  )
+}
+
+export default HomePage
