89f5309decbfaf55557fb2241e8798f02d532d9b
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-03 13:52:15 +0100

    Refactor signup page to handle loading and error states

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -1,7 +1,5 @@
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-export const API_URL = 'http://localhost:3000';
-export const API_KEY = '';
-export const handleToast = (error) => {
-  toast(error, { position: 'top-center', type: 'error', autoClose: 5000 });
-};
+export const API_URL = "http://localhost:3000";
+export const API_KEY = "";
+export const handleToast = (error) => toast(error, { position: "top-center", type: "error", autoClose: 5000 });
