615e0a77b69fb8da32727d21f4de78f86bc58260
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-05 11:43:35 +0100

    Remove register slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from './loginSlice';
-import registerSlice from './registerSlice';
 import carSlice from "./carSlice";
 
 const store = configureStore({
   reducer: {
     user: loginSlice.reducer,
-    register: registerSlice.reducer,
     cars: carSlice.reducer,
   },
 
