dadc81642f770ed2d59f5b116baf61ec0f3651f4
Eyasu Teshome <joshtesh91@mail.com>
2022-12-05 23:30:37 +0800

    add a form for new car

---
+++ src/components/AddCar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './AddCar.css';
+
+function AddCar() {
+  return (
+    <div className="add-car">
+      <Navbar />
+      <div className="layer">
+        <div className="form-container">
+          <div className="form">
+            <h1>Add A New Car</h1>
+            <form action="">
+              <input
+                className=""
+                type="text"
+                // value={}
+                // onChange
+                placeholder="Name"
+              />
+              <input
+                className=""
+                type="text"
+                // value={}
+                // onChange
+                placeholder="Brand"
+              />
+              <input
+                className=""
+                type="number"
+                // value={}
+                // onChange
+                placeholder="Option to purchase fee"
+              />
+              <input
+                className=""
+                type="number"
+                // value={}
+                // onChange
+                placeholder="Total amount payable"
+              />
+              <input
+                className=""
+                type="number"
+                // value={}
+                // onChange
+                placeholder="Duration"
+              />
+              <label htmlFor="image">
+                Upload Image
+                <input
+                  className="image"
+                  type="file"
+                  // value={}
+                  // onChange
+                  placeholder=""
+                />
+              </label>
+              <button className="submit-btn" type="submit">Add Car</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddCar;
