a0ce7a9f7432220e688039965e3a6e6bdc6e96a9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-06 15:35:15 +0000

    Add logic to add a new car using redux

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,59 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { SpinnerRoundOutlined } from 'spinners-react';
+import { ToastContainer } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+import { getCarsStatus, getCarsError, createCar } from '../redux/carSlice';
 import Navbar from './Navbar';
 import './AddCar.css';
+import { handleToast } from '../redux/utils';
 
 function AddCar() {
+  const [name, setName] = useState('');
+  const [brand, setBrand] = useState('');
+  const [optionToPurchaseFee, setOptionToPurchaseFee] = useState(0);
+  const [totalAmountPayable, setTotalAmountPayable] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [image, setImage] = useState('');
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const status = useSelector(getCarsStatus);
+  const error = useSelector(getCarsError);
+  let isLoading = false;
+
+  useEffect(() => {
+    if (status === 'loading') {
+      isLoading = true;
+    }
+    if (error) handleToast(error);
+  }, [status, dispatch]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      brand,
+      optionToPurchaseFee,
+      totalAmountPayable,
+      duration,
+      image,
+    };
+    console.log(data, 'newly created data');
+    dispatch(createCar(data));
+    navigate('/');
+  };
+
   return (
     <div className="add-car">
       <Navbar />
+      <ToastContainer />
       <div className="layer">
         <div className="form-container">
           <div className="form">
             <h1>Add A New Car</h1>
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <input
                 className=""
                 type="text"
-                // value={}
-                // onChange
+                value={name}
+                onChange={(e) => setName(e.valueue)}
                 placeholder="Name"
               />
               <input
                 className=""
                 type="text"
-                // value={}
-                // onChange
+                value={brand}
+                onChange={(e) => setBrand(e.value)}
                 placeholder="Brand"
               />
               <input
                 className=""
                 type="number"
-                // value={}
-                // onChange
+                value={optionToPurchaseFee}
+                onChange={(e) => setOptionToPurchaseFee(e.value)}
                 placeholder="Option to purchase fee"
               />
               <input
                 className=""
                 type="number"
-                // value={}
-                // onChange
+                value={totalAmountPayable}
+                onChange={(e) => setTotalAmountPayable(e.value)}
                 placeholder="Total amount payable"
               />
               <input
                 className=""
                 type="number"
-                // value={}
-                // onChange
+                value={duration}
+                onChange={(e) => setDuration(e.value)}
                 placeholder="Duration"
               />
               <input
                 className=""
                 type="text"
-                // value={}
-                // onChange
+                value={image}
+                onChange={(e) => setImage(e.value)}
                 placeholder="Image Link"
               />
-              <button className="submit-btn" type="submit">Add Car</button>
+              <button className="submit-btn" type="submit">
+                {isLoading ? (
+                  <SpinnerRoundOutlined color="black" size={100} />
+                ) : (
+                  'Add Car'
+                )}
+              </button>
             </form>
           </div>
         </div>
