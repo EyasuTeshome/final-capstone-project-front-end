809b5691303fb1fcadf3c958f32c7b4f79527f73
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-07 04:09:52 +0000

    Handle car creation with react form hook

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,21 +1,44 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { SpinnerRoundOutlined } from 'spinners-react';
 import { ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import { getCarsStatus, getCarsError, createCar } from '../redux/carSlice';
 import Navbar from './Navbar';
 import './AddCar.css';
 import { handleToast } from '../redux/utils';
 
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    brand: yup.string().required(),
+    image: yup.string().required(),
+    duration: yup.number().positive().integer().required(),
+    totalAmountPayable: yup.number().positive().integer().required(),
+    optionToPurchaseFee: yup.number().positive().integer().required(),
+  })
+  .required();
+
 function AddCar() {
-  const [name, setName] = useState('');
-  const [brand, setBrand] = useState('');
-  const [optionToPurchaseFee, setOptionToPurchaseFee] = useState(0);
-  const [totalAmountPayable, setTotalAmountPayable] = useState(0);
-  const [duration, setDuration] = useState(0);
-  const [image, setImage] = useState('');
+  // const [name, setName] = useState('');
+  // const [brand, setBrand] = useState('');
+  // const [optionToPurchaseFee, setOptionToPurchaseFee] = useState(0);
+  // const [totalAmountPayable, setTotalAmountPayable] = useState(0);
+  // const [duration, setDuration] = useState(0);
+  // const [image, setImage] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -30,20 +53,20 @@ function AddCar() {
     if (error) handleToast(error);
   }, [status, dispatch]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      name,
-      brand,
-      optionToPurchaseFee,
-      totalAmountPayable,
-      duration,
-      image,
-    };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const data = {
+  //     name,
+  //     brand,
+  //     optionToPurchaseFee,
+  //     totalAmountPayable,
+  //     duration,
+  //     image,
+  //   };
 
-    dispatch(createCar(data));
-    navigate('/');
-  };
+  //   dispatch(createCar(data));
+  //   navigate('/');
+  // };
 
   return (
     <div className="add-car">
@@ -53,49 +76,31 @@ function AddCar() {
         <div className="form-container">
           <div className="form">
             <h1>Add A New Car</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input {...register('name')} placeholder="Name" />
+              <p>{errors.name?.message}</p>
+
+              <input {...register('brand')} placeholder="brand" />
+              <p>{errors.brand?.message}</p>
+
               <input
-                className=""
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.valueue)}
-                placeholder="Name"
+                {...register('optionToPurchaseFee')}
+                placeholder="optionToPurchaseFee"
               />
+              <p>{errors.optionToPurchaseFee?.message}</p>
+
               <input
-                className=""
-                type="text"
-                value={brand}
-                onChange={(e) => setBrand(e.value)}
-                placeholder="Brand"
+                {...register('totalAmountPayable')}
+                placeholder="totalAmountPayable"
               />
-              <input
-                className=""
-                type="number"
-                value={optionToPurchaseFee}
-                onChange={(e) => setOptionToPurchaseFee(e.value)}
-                placeholder="Option to purchase fee"
-              />
-              <input
-                className=""
-                type="number"
-                value={totalAmountPayable}
-                onChange={(e) => setTotalAmountPayable(e.value)}
-                placeholder="Total amount payable"
-              />
-              <input
-                className=""
-                type="number"
-                value={duration}
-                onChange={(e) => setDuration(e.value)}
-                placeholder="Duration"
-              />
-              <input
-                className=""
-                type="text"
-                value={image}
-                onChange={(e) => setImage(e.value)}
-                placeholder="Image Link"
-              />
+              <p>{errors.totalAmountPayable?.message}</p>
+
+              <input {...register('duration')} placeholder="duration" />
+              <p>{errors.duration?.message}</p>
+
+              <input {...register('image')} placeholder="image" />
+              <p>{errors.image?.message}</p>
+
               <button className="submit-btn" type="submit">
                 {isLoading ? (
                   <SpinnerRoundOutlined color="black" size={100} />
