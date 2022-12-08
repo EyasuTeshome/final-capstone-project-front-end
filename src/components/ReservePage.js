b80c63e8d261e3297beabac045d621fceff9ae01
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-08 10:01:08 +0100

    Successfully submit a reservation

--- src/components/ReservePage.js
+++ src/components/ReservePage.js
@@ -1,19 +1,63 @@
 import { useState } from "react";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { handleToast } from "../redux/utils";
 import "./reservations.css";
 
 export default function ReservePage() {
   const CITIES = ["London", "New York", "Berlin", "Paris"];
   // const { cars } = useSelector((state) => state.cars);
-  const cars = [{ name: "bugatti", id: 1 }];
+  const cars = [{ name: "bugatti", id: 6 }];
+  const { data } = useSelector((state) => state.user);
   const [city, setCity] = useState("London");
   const [car, setCar] = useState(cars[0].id);
-  const [date, setDate] = useState("");
+  const dateObject = new Date();
+  dateObject.setDate(dateObject.getDate());
+  const today = dateObject.toISOString().substring(0, 10);
+  const [date, setDate] = useState(today);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/reservations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          reservation: {
+            user_id: data.id,
+            city,
+            car_id: car,
+            date,
+          },
+        }),
+      });
+
+      const msg = await res.json();
+
+      if (res.status === 201) {
+        handleToast("Created Reservation Succesfully");
+      } else {
+        handleToast(
+          <p>
+            {msg.error}
+            <br />
+            {msg.exception}
+          </p>
+        );
+      }
+    } catch (err) {
+      handleToast(err.message);
+    }
+  };
 
   return (
     <div className="reserve-background">
       <div className="reserve-layer">
         <div className="reserve-page">
+          <ToastContainer />
           <h1 className="reserve-title">BOOK A TESLA TEST - RIDE</h1>
           <p className="reserve-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -24,7 +68,7 @@ export default function ReservePage() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <form className="reserve-form">
+          <form className="reserve-form" onSubmit={handleSubmit}>
             <select
               value={city}
               onChange={(e) => setCity(e.target.value)}
@@ -58,7 +102,9 @@ export default function ReservePage() {
               onChange={(e) => setDate(e.target.value)}
             />
 
-            <button className="reserve-btn" type="button">Book Now</button>
+            <button className="reserve-btn" type="submit">
+              Book Now
+            </button>
           </form>
         </div>
       </div>
