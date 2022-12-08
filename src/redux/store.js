9b7eeacd549ba912b4c29aae295496ede12b5e17
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-08 16:35:14 +0000

    Add reservations to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import loginSlice from './loginSlice';
-import carSlice from "./carSlice";
+import carSlice from './carSlice';
+import reservationSlice from './reservationSlice';
 
 const store = configureStore({
   reducer: {
     user: loginSlice.reducer,
     cars: carSlice.reducer,
+    reservations: reservationSlice.reducer,
   },
-
 });
 
 export default store;
