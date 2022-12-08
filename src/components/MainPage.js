b3aabea4fbbde026090914aa175bfd8209d86460
Eyasu Teshome <joshtesh91@mail.com>
2022-12-08 23:05:37 +0800

    add slick settings

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -65,4 +65,39 @@ function MainPage() {
     ));
   }
 
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   
