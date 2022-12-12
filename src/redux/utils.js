c98335403e3e2de03345fae5d7f5b457c47c9db9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-12 06:37:45 +0000

    Add a type parameter to toast

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -2,4 +2,4 @@ import { toast } from "react-toastify";
 
 export const API_URL = "http://localhost:3000";
 export const API_KEY = "";
-export const handleToast = (error) => toast(error, { position: "top-center", type: "error", autoClose: 5000 });
+export const handleToast = (error, type = "success") => toast(error, { position: "top-center", type, autoClose: 5000 });
