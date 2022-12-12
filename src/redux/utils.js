5b653e8742e7d0276444a3513040d3e2e5bcb9d9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-12 06:44:34 +0000

    Update toast function

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -2,4 +2,4 @@ import { toast } from "react-toastify";
 
 export const API_URL = "http://localhost:3000";
 export const API_KEY = "";
-export const handleToast = (error, type = "success") => toast(error, { position: "top-center", type, autoClose: 5000 });
+export const handleToast = ({ msg, type = "success" }) => toast(msg, { position: "top-center", type, autoClose: 5000 });
