30a3700ccb12871e4a752da8239b86976c8b5c26
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-12-16 19:06:40 +0200

    Update main.js

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,12 +1,4 @@
 
-            <div className="price even">
-              <p>Finance Fee</p>
-              <p>$129</p>
-            </div>
-            <p>
-              <span>5.9% APR </span>
-              representative
-            </p>
             <button type="submit">View Details</button>
           </section>
         </div>
