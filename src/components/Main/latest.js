ca73cecaec8233559ef7d34cf5d7f81993bbbac9
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-12-16 19:04:11 +0200

    Update latest.js

--- src/components/Main/latest.js
+++ src/components/Main/latest.js
@@ -1,16 +1,4 @@
-
-                <div className="item">
-                  <img src={img1} alt="car" />
-                  <h5>car</h5>
-                </div>
-                <div className="item">
-                  <img src={img6} alt="car" />
-                  <h5>car</h5>
-                </div>
-              </Carousel>
-            </div>
-          </div>
-        </div>
+       </div>
       </div>
     </section>
   );
