2131c70c5218eefc226f2f3bb8aa0acebe4f8393
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-12-16 19:06:20 +0200

    Update main.js

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,15 +1,4 @@
-e="submit">View Details</button>
-          </section>
-        </div>
-        <div className="main">
-          <img src={img3} alt="car2" />
-          <section className="car-info">
-            <h2>car 1</h2>
-            <p>car 1 details</p>
-            <div className="price">
-              <p>Finance Fee</p>
-              <p>$129</p>
-            </div>
+
             <div className="price even">
               <p>Finance Fee</p>
               <p>$129</p>
