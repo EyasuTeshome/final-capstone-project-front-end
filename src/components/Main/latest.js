d790146136b3079b4ade27e2d7245641caaf94d6
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-12-16 19:10:43 +0200

    Update latest.js

--- src/components/Main/latest.js
+++ src/components/Main/latest.js
@@ -1,22 +1,4 @@
 
-    <section className="latest">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="skill-bx wow zoomIn">
-              <h1>Latest</h1>
-              <hr />
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. my text.
-              </p>
-              <Carousel responsive={responsive} infinite className="owl-carousel owl-theme skill-slider">
-                <div className="item">
-                  <img src={img1} alt="car" />
-                  <h5>Car 1</h5>
-                </div>
-                <div className="item">
-                  <img src={img2} alt="car" />
-                  <h5>Car2</h5>
                 </div>
                 <div className="item">
                   <img src={img3} alt="car" />
